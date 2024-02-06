6df8fc25a3992ad485e4fe141d5f17806606ea69
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-07 08:32:01 +0900

    ✨ feat : Authページ専用イメージコンポネント化

---
+++ components/Grass.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet } from 'react-native'
+
+const Grass = (): JSX.Element => {
+  return (
+    <Image source={{ uri: 'https://i.ibb.co/NC5JrQ1/grass.png' }} style={styles.grass} />
+  )
+}
+
+const styles = StyleSheet.create({
+  grass: {
+    width: '100%',
+    height: 200,
+    alignSelf: 'center'
+  }
+})
+
+export default Grass

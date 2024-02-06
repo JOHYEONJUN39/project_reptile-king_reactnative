8d0c6a5eabdcf42bb18f30e47e33e0592d57087b
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-07 08:32:26 +0900

    ✨ feat : 一般Header作成

---
+++ components/header/CommonHeader.tsx
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+interface BackHeaderProps {
+  title: string
+};
+
+const CommonHeader = ({ title }: BackHeaderProps): JSX.Element => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerInner}>
+        <Text style={styles.headerTitle}>{title}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#072E0A',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
+    alignItems: 'center'
+  },
+  headerTitle: {
+    paddingBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  }
+})
+
+export default CommonHeader

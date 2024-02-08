ca939783abc5abb33c87674eb711750cbf94c1ae
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-08 17:46:26 +0900

    ✨ feat : メニューのスタイリング及びカテゴリー配置

--- pages/Menu.tsx
+++ pages/Menu.tsx
@@ -1,9 +1,54 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet, ScrollView, Text } from 'react-native'
+import Category from '../components/Category'
+import type { CategoryList } from '../types/CategoryList'
+import CategoryData from '../assets/categoryData.json'
 
 const Menu = (): JSX.Element => {
+  const categories: CategoryList[] = CategoryData
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#072E0A',
+      justifyContent: 'space-between'
+    },
+    categoryBox: {
+      paddingTop: 12,
+      paddingBottom: 12,
+      paddingHorizontal: 27,
+      flexWrap: 'wrap',
+      flexDirection: 'row',
+      backgroundColor: '#1C5B20'
+    },
+    shopping: {
+      paddingTop: 12,
+      paddingHorizontal: 20,
+      alignItems: 'flex-start',
+      backgroundColor: '#1C5B20'
+    },
+    text: {
+      color: 'white',
+      fontSize: 24,
+      fontWeight: 'bold'
+    }
+  })
+
   return (
-    <View>
-      <Text>메뉴</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.shopping}>
+          <Text style={styles.text} >쇼핑</Text>
+        </View>
+        <View style={styles.categoryBox}>
+          {categories.map((category, index) => (
+          <Category
+            key={index}
+            name={category.name}
+            image={category.image}
+          />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }

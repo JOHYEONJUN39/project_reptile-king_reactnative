8bbcab8f9440d4ccefcdd60030db9ead6de9f7da
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-16 15:09:48 +0900

    ✨ feat : 商品臨時データ作成、適用

--- pages/Market.tsx
+++ pages/Market.tsx
@@ -1,7 +1,48 @@
-import { View, StyleSheet, ScrollView, TouchableOpacity, useWindowDimensions, Image } from 'react-native'
+import { View, StyleSheet, ScrollView, TouchableOpacity, Text, useWindowDimensions, Image } from 'react-native'
 import type { CategoryList } from '../types/CategoryList'
 import Category from '../components/Category'
 import CategoryData from '../assets/categoryData.json'
+import Line from '../components/Line'
+import ProductBox from '../components/ProductBox'
+
+const data = [
+  {
+    id: 1,
+    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 사육장 세트',
+    image: 'https://i.postimg.cc/W3zLYSvP/image.png',
+    price: '₩ 35,000'
+  },
+  {
+    id: 2,
+    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 사육장',
+    image: 'https://i.postimg.cc/sx1jSsX7/image.jpg',
+    price: '₩ 35,000'
+  },
+  {
+    id: 3,
+    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 채집통',
+    image: 'https://i.postimg.cc/0Q4gTmkK/image.png',
+    price: '₩ 35,000'
+  },
+  {
+    id: 4,
+    title: '이탈리아 베네치아 장인이 손수 열심히 제작한 바닥재',
+    image: 'https://i.postimg.cc/BQrSLWGP/image.jpg',
+    price: '₩ 35,000'
+  },
+  {
+    id: 5,
+    title: '한마리 한마리 열심히 사육한 밀웜',
+    image: 'https://i.postimg.cc/hGqtYkyR/image.jpg',
+    price: '₩ 35,000'
+  },
+  {
+    id: 6,
+    title: '이탈리아 베네치아 야자수',
+    image: 'https://i.postimg.cc/T3vx2rrr/image.png',
+    price: '₩ 35,000'
+  }
+]
 
 const Market = (): JSX.Element => {
   const windowWidth = useWindowDimensions().width
@@ -16,7 +57,6 @@ const Market = (): JSX.Element => {
     },
     inner: {
       paddingVertical: 24,
-      paddingHorizontal: 27,
       alignItems: 'center'
     },
     title: {
@@ -25,18 +65,30 @@ const Market = (): JSX.Element => {
       marginBottom: 24
     },
     eventBox: {
-      width: windowWidth * 0.8,
+      width: windowWidth * 0.9,
       height: 120,
       backgroundColor: '#fff',
       borderRadius: 16,
       marginBottom: 24,
-      overflow: 'hidden',
-      alignItems: 'center'
+      overflow: 'hidden'
     },
     categoryScrollView: {
       minWidth: windowWidth * 0.8,
       flexDirection: 'row',
       overflow: 'scroll'
+    },
+    offerTitle: {
+      fontSize: 24,
+      fontWeight: '400',
+      marginBottom: 24,
+      color: '#fff'
+    },
+    offerBox: {
+      width: windowWidth * 0.9,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      flexWrap: 'wrap',
+      marginBottom: 24
     }
   })
 
@@ -55,6 +107,13 @@ const Market = (): JSX.Element => {
           />
           ))}
         </ScrollView>
+        <Line color='#39823E' weight={4} mV={12} />
+        <Text style={styles.offerTitle}>오늘의 추천 상품</Text>
+        <View style={styles.offerBox}>
+          {data.map((item, index) => (
+            <ProductBox key={index} title={item.title} image={item.image} price={item.price}/>
+          ))}
+        </View>
       </ScrollView>
     </View>
   )

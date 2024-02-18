f664e8deda7d1bd29ce69d42fef385a6e4e33579
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-18 23:07:57 +0900

    ✨ feat : 臨時データ使用

--- pages/ProductsByCategory.tsx
+++ pages/ProductsByCategory.tsx
@@ -1,8 +1,10 @@
 import { useRoute } from '@react-navigation/native'
 import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import type { ProductRouteProp } from '../types/RootStackParamList'
+import Product from '../components/Product'
+import productData from '../assets/ProductData.json'
 
-const Product = (): JSX.Element => {
+const ProductsByCategory = (): JSX.Element => {
   const route = useRoute<ProductRouteProp>()
   const { category } = route.params
 
@@ -13,24 +15,28 @@ const Product = (): JSX.Element => {
       justifyContent: 'space-between'
     },
     inner: {
-      paddingVertical: 24,
-      paddingHorizontal: 27,
-      alignItems: 'center'
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      flexWrap: 'wrap'
     },
     category: {
       fontSize: 18,
       fontWeight: 'bold',
-      color: '#fff'
+      color: '#fff',
+      paddingVertical: 10,
+      paddingLeft: 4
     }
   })
 
   return (
     <View style={styles.container}>
+      <Text style={styles.category}>{category}</Text>
       <ScrollView contentContainerStyle={styles.inner}>
-        <Text style={styles.category}>{category}</Text>
+        {productData.map(product => <Product key={product.code} product={product} />)}
       </ScrollView>
     </View>
   )
 }
 
-export default Product
+export default ProductsByCategory

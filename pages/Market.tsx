4523a508e3da94949490ea440fa6bf495bc6a250
NEMURENAIYORU <lhslhs301@gmail.com>
2024-02-22 20:58:06 +0900

    ✨ feat : おすすめ商品から商品詳細へナビゲーション実装

--- pages/Market.tsx
+++ pages/Market.tsx
@@ -4,45 +4,7 @@ import Category from '../components/Category'
 import shoppingData from '../assets/shoppingData.json'
 import Line from '../components/Line'
 import ProductBox from '../components/ProductBox'
-
-const data = [
-  {
-    id: 1,
-    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 사육장 세트',
-    image: 'https://i.postimg.cc/W3zLYSvP/image.png',
-    price: '₩ 35,000'
-  },
-  {
-    id: 2,
-    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 사육장',
-    image: 'https://i.postimg.cc/sx1jSsX7/image.jpg',
-    price: '₩ 35,000'
-  },
-  {
-    id: 3,
-    title: '이탈리아 베네치아 장인이 손수 한땀한땀 열심히 제작한 채집통',
-    image: 'https://i.postimg.cc/0Q4gTmkK/image.png',
-    price: '₩ 35,000'
-  },
-  {
-    id: 4,
-    title: '이탈리아 베네치아 장인이 손수 열심히 제작한 바닥재',
-    image: 'https://i.postimg.cc/BQrSLWGP/image.jpg',
-    price: '₩ 35,000'
-  },
-  {
-    id: 5,
-    title: '한마리 한마리 열심히 사육한 밀웜',
-    image: 'https://i.postimg.cc/hGqtYkyR/image.jpg',
-    price: '₩ 35,000'
-  },
-  {
-    id: 6,
-    title: '이탈리아 베네치아 야자수',
-    image: 'https://i.postimg.cc/T3vx2rrr/image.png',
-    price: '₩ 35,000'
-  }
-]
+import productData from '../assets/ProductData.json'
 
 const Market = (): JSX.Element => {
   const windowWidth = useWindowDimensions().width
@@ -110,8 +72,11 @@ const Market = (): JSX.Element => {
         <Line color='#39823E' weight={4} mV={12} />
         <Text style={styles.offerTitle}>오늘의 추천 상품</Text>
         <View style={styles.offerBox}>
-          {data.map((item, index) => (
-            <ProductBox key={index} title={item.title} image={item.image} price={item.price}/>
+          {productData.map((product, index) => (
+          <ProductBox
+            key={index}
+            product={product}
+          />
           ))}
         </View>
       </ScrollView>

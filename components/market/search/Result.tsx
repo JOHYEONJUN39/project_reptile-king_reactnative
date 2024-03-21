809549417e205eb030b0dab119d693922f00f56e
NEMURENAIYORU <lhslhs301@gmail.com>
2024-03-21 20:54:32 +0900

    ✨ feat : 検索して出た結果で商品画面に移動するように変更

--- components/market/search/Result.tsx
+++ components/market/search/Result.tsx
@@ -1,16 +1,23 @@
 import { FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import type { ProductNavigationProp } from '../../../types/RootStackParamList'
 
 interface ResultProps {
   text: string
   searchQuery: string
+  code: string
 }
 
-const Result = ({ text, searchQuery }: ResultProps): JSX.Element => {
+const Result = ({ text, searchQuery, code }: ResultProps): JSX.Element => {
+  const navigation = useNavigation<ProductNavigationProp>()
+  const navigateProduct = (): void => {
+    navigation.navigate('Product', { productCode: code })
+  }
   const parts = text !== '' ? text.split(new RegExp(`(${searchQuery})`, 'gi')) : []
   return (
-    <TouchableOpacity style={styles.resultContainer}>
+    <TouchableOpacity style={styles.resultContainer} onPress={navigateProduct}>
       <FontAwesome name="search" size={20} color="#fff" style={{ marginHorizontal: 8 }}/>
       <Text style={{ color: '#fff', fontWeight: 'bold' }}>
         {parts.map((part, index) => (
